1085b1b77d6e8803ecafe63b2049260fef502c02
charly2790 <carlosba90@gmail.com>
2022-07-25 21:50:22 -0300

    prueba

---
+++ EASTeam/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {'Prueba':'Hola Mundo'}
+    return render(request,'index.html',context = context)
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# EASTeam-Project
-Sistema web de gestión integral de actividad de equipo de running.
