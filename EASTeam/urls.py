419760e0cdbb45ad104296e155001a701196151a
charly2790 <carlosba90@gmail.com>
2022-08-15 23:17:50 -0300

    'users-app-commit'

--- EASTeam/urls.py
+++ EASTeam/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from EASTeam.views import index
+from Users.views import user_register_view
 
 urlpatterns = [
     path('',index,name='index'),
     path('admin/', admin.site.urls),
+    path('users/',include('Users.urls')),
     
 ]
