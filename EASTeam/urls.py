cf908f76687cfe251aee25884466b640f117a7d5
Gaston Leegstra <gastonleegstra@gmail.com>
2022-08-18 20:18:07 -0300

    se corrige urls.py del EASTeam

--- EASTeam/urls.py
+++ EASTeam/urls.py
@@ -21,10 +21,7 @@ from Users.views import user_register_view
 urlpatterns = [
     path('',index,name='index'),
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('accounts/', include('allauth.urls')),
-=======
     path('users/',include('Users.urls')),
     
->>>>>>> a301b9eb563dc2cb8fe93aaa925bbbd08763f7fe
 ]
