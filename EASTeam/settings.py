8133a081bf636b00747377fa88e5307c6c76f4e2
Gaston Leegstra <gastonleegstra@gmail.com>
2022-09-15 14:14:47 -0300

    se modifica settings.py

--- EASTeam/settings.py
+++ EASTeam/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY= env.str('SECRET_KEY')
+SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
