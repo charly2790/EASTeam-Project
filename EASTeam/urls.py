091f9c62d54bf8608c601fe062408b6df330bea7
Gaston Leegstra <gastonleegstra@gmail.com>
2022-09-17 19:43:48 -0300

    se modifica url de acceso, se modifica pie, cabecera, boton de whatsapp, pagina base

--- EASTeam/urls.py
+++ EASTeam/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('',index,name='index'),
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('users/',include('Users.urls')),
     
 ]
--- Users/urls.py
+++ Users/urls.py
@@ -2,6 +2,5 @@ from django.urls import path,include
 from Users.views import dashboard
 
 urlpatterns = [
-    path('', include('allauth.urls')),
     path('dashboard/',dashboard,name ='dashboard'),
     ]
