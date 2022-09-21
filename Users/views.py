6be16157141a767b60cdc5800d876f7bfc75930b
Gaston Leegstra <gastonleegstra@gmail.com>
2022-09-21 19:04:09 -0300

    cambios en view.py y signup.html

--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 from Users.forms import user_register_form
 
 
@@ -13,7 +12,6 @@ comentado
         context = {'form': form}
         return render(request,'Users/user_register_template.html',context = context)
 '''
-@login_required
 def dashboard(request):
     user = request.user
     context = {}
